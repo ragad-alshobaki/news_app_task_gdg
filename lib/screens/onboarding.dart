67d5274e2fec1e9b141699faeda98abe8ac89000
ragad-alshobaki <rgd.alshobaki@gmail.com>
2025-07-09 02:06:30 +0300

    OnboardingScreen Done

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/util/app_assets.dart';
+import 'package:news_app/util/app_color.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -6,7 +8,33 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage(AppAssets.onBoardingImg),
+          fit: BoxFit.cover)
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.newspaper,
+                size: 60,
+                color: AppColor.primaryColor,  
+              ),
+              SizedBox(height: 20,),
+              Text("Welcom to News app",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColor.greyColor,
+                // fontSize: 30,
+              ),
+              ),
+              SizedBox(height: 20,),
+              FilledButton(onPressed: (){}, child: Text('Get Started'))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/util/app_assets.dart
@@ -0,0 +1,5 @@
+
+abstract class AppAssets {
+  static const String onBoardingImg = 'assets/images/buildings.png';
+  // static const Icons onBoardingIcon = Icons.newspaper;
+}
